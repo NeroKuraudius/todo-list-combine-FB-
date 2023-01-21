c3276b3c02055ea1f67f4d614e3bd2eab82e5f55
Nero <karta0706566@gmail.com>
2023-01-22 07:57:07 +0800

    feat: add create function

--- app.js
+++ app.js
@@ -6,8 +6,10 @@ if (process.env.NODE_ENV !== 'production') {
 
 const app = express()
 const exphbs = require('express-handlebars')
+const bodyParser = require('body-parser')
 mongoose.connect(process.env.MONGODB_URI, { useNewUrlParser: true, useUnifiedTopology: true })
 
+// 引用todo.js中的 Schema
 const Todo = require('./models/todo')
 
 const db = mongoose.connection
@@ -24,11 +26,24 @@ db.once('open', () => {
 app.engine('hbs', exphbs.engine({ extname: '.hbs', defaultLayout: 'main' }))
 app.set('view engine', 'hbs')
 
+app.use(bodyParser.urlencoded({ extended: true }))
+
 app.get('/', (req, res) => {
   Todo.find() // 取出Todo model中的資料
     .lean() // ※point※把Mongoose的Model物件換成乾淨的JS資料
     .then(todos => { res.render('index', { todos }) }) // {todos} = { todos : todos }
     .catch(error => { console.error(error) }) //錯誤捕捉
+})
+
+app.get('/todos/new', (req, res) => {
+  return res.render('new')
+})
+
+app.post('/todos', (req, res) => {
+  const name = req.body.name
+  return Todo.create({ name }) //存回資料庫
+    .then(() => { res.redirect('/') }) //新增厚導回首頁
+    .catch(error => { console.log(error) })
 })
 
 app.listen(3000, () => {
