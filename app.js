ff9fc0deeb299ad333492fa840b1a188a57c14a8
Nero <karta0706566@gmail.com>
2023-01-21 22:28:41 +0800

    feat: add the express-handlebars

--- app.js
+++ app.js
@@ -5,6 +5,7 @@ if (process.env.NODE_ENV !== 'production') {
 }
 
 const app = express()
+const exphbs = require('express-handlebars')
 mongoose.connect(process.env.MONGODB_URI, { useNewUrlParser: true, useUnifiedTopology: true })
 
 const db = mongoose.connection
@@ -17,8 +18,12 @@ db.once('open', () => {
   console.log('mongodb connected!')
 })
 
+// extname:指定副檔名為縮寫的hbs
+app.engine('hbs', exphbs.engine({ extname: '.hbs' ,defaultLayout: 'main'}))
+app.set('view engine', 'hbs')
+
 app.get('/', (req, res) => {
-  res.send('<h2>Hello, world!</h2>')
+  res.render('index')
 })
 
 app.listen(3000, () => {
