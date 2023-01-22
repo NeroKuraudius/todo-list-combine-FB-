98808e70ba45f3e255a0b2f71f11715c113e3cbc
Nero <karta0706566@gmail.com>
2023-01-22 09:57:34 +0800

    feat: update the edit function

--- app.js
+++ app.js
@@ -28,6 +28,7 @@ app.set('view engine', 'hbs')
 
 app.use(bodyParser.urlencoded({ extended: true }))
 
+//主頁
 app.get('/', (req, res) => {
   Todo.find() // 取出Todo model中的資料
     .lean() // ※point※把Mongoose的Model物件換成乾淨的JS資料
@@ -35,10 +36,12 @@ app.get('/', (req, res) => {
     .catch(error => { console.error(error) }) //錯誤捕捉
 })
 
+// 新增頁
 app.get('/todos/new', (req, res) => {
   return res.render('new')
 })
 
+// 資料儲存
 app.post('/todos', (req, res) => {
   const name = req.body.name
   return Todo.create({ name }) //存回資料庫
@@ -46,12 +49,34 @@ app.post('/todos', (req, res) => {
     .catch(error => { console.log(error) })
 })
 
+// 特定資料瀏覽
 app.get('/todos/:id', (req, res) => {
   const id = req.params.id
   return Todo.findById(id)
     .lean()
     .then(todo => res.render('detail', { todo }))
     .catch(error => { console.log(error) })
+})
+
+// 資料編輯頁
+app.get('/todos/:id/edit', (req, res) => {
+  const id = req.params.id
+  return Todo.findById(id)
+    .lean()
+    .then((todo) => res.render('edit', { todo }))
+    .catch(error => console.log(error))
+})
+
+// 資料編輯後儲存
+app.post('/todos/:id/edit',(req,res)=>{
+  const id = req.params.id
+  const name = req.body.name
+  return Todo.findById(id)
+    .then(todo=>{todo.name = name
+    return todo.save()
+    })
+    .then(()=>{res.redirect(`/todos/${id}`)})
+    .catch(error=>console.log(error))
 })
 
 app.listen(3000, () => {
