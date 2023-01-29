f50c224713bebc29c12db97bebfdc68987bf12dd
Nero <karta0706566@gmail.com>
2023-01-29 15:36:42 +0800

    feat: add the checkbox

--- app.js
+++ app.js
@@ -70,16 +70,19 @@ app.get('/todos/:id/edit', (req, res) => {
 // 資料編輯後儲存
 app.post('/todos/:id/edit', (req, res) => {
   const id = req.params.id
-  const name = req.body.name
+  const { name, isDone } = req.body
+
   return Todo.findById(id)
     .then(todo => {
       todo.name = name
+      todo.isDone = isDone === 'on'
       return todo.save()
     })
     .then(() => { res.redirect(`/todos/${id}`) })
     .catch(error => console.log(error))
 })
 
+// 資料刪除
 app.post('/todos/:id/delete', (req, res) => {
   const id = req.params.id
   return Todo.findById(id)
