757c06252bb233aa61403e384d15e38bd85db63b
Nero <karta0706566@gmail.com>
2023-03-17 21:43:19 +0800

    feat: add the logout feature

--- config/passport.js
+++ config/passport.js
@@ -3,35 +3,34 @@ const LocalStrategy = require('passport-local').Strategy
 
 const User = require('../models/user')
 
-module.exports = app =>{
+module.exports = app => {
   // 初始化 passport模組
   app.use(passport.initialize())
   app.use(passport.session())
 
   // 設定登入策略：本地策略
-  passport.use(new LocalStrategy({usernameField:'email'},(email,password,done)=>{
-    User.findOne({email})
-    .then(user=>{
-      if (!user){
-        return done(null,false,{message:'This email is not registered.'})
-      }
-      if (user.password !== password){
-        return done(null,false,{message:'The password is wrong.'})
-      }
-      return done(null,user)
-    })
-    .catch(error=>done(err,false))
+  passport.use(new LocalStrategy({ usernameField: 'email' }, (email, password, done) => {
+    User.findOne({ email })
+      .then(user => {
+        if (!user) {
+          return done(null, false, { message: 'This email is not registered.' })
+        }
+        if (user.password !== password) {
+          return done(null, false, { message: 'The password is wrong.' })
+        }
+        return done(null, user)
+      })
+      .catch(error => done(err, false))
   }))
 
   // 設定序列化、反序列化
-  passport.serializeUser((user,done)=>{
-    console.log(user)
-    done(null,user._id)
+  passport.serializeUser((user, done) => {
+    done(null, user._id)
   })
-  passport.deserializeUser((id,done)=>{
+  passport.deserializeUser((id, done) => {
     User.findById(id)
-    .lean()
-    .then(user=>done(null,user))
-    .catch(err=>done(err))
+      .lean()
+      .then(user => done(null, user))
+      .catch(err => done(err))
   })
 } 
--- routes/modules/users.js
+++ routes/modules/users.js
@@ -7,10 +7,15 @@ router.get('/login', (req, res) => {
   res.render('login')
 })
 
-router.post('/login', passport.authenticate('local',{
-  successRedirect:'/',
-  failureRedirect:'/users/login'
+router.post('/login', passport.authenticate('local', {
+  successRedirect: '/',
+  failureRedirect: '/users/login'
 }))
+
+router.get('/logout', (req, res) => {
+  req.logout()
+  res.redirect('/users/login')
+})
 
 router.get('/register', (req, res) => {
   res.render('register')
