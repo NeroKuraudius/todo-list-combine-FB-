722001f249efbb6efdb49de69dc70a3e357ef1dc
Nero <karta0706566@gmail.com>
2023-03-20 20:16:05 +0800

    feat: add relations between User and Todo models

--- routes/modules/todos.js
+++ routes/modules/todos.js
@@ -11,16 +11,18 @@ router.get('/new', (req, res) => {
 
 // 資料儲存
 router.post('/', (req, res) => {
+  const userId = req.user._id
   const name = req.body.name
-  return Todo.create({ name }) //存回資料庫
+  return Todo.create({ name, userId }) //存回資料庫
     .then(() => { res.redirect('/') }) //新增後導回首頁
     .catch(error => { console.log(error) })
 })
 
 // 特定資料瀏覽
 router.get('/:id', (req, res) => {
+  const userId = req.user._id
   const id = req.params.id
-  return Todo.findById(id)
+  return Todo.findOne({ id, userId })
     .lean()
     .then(todo => res.render('detail', { todo }))
     .catch(error => { console.log(error) })
@@ -28,8 +30,9 @@ router.get('/:id', (req, res) => {
 
 // 資料編輯頁
 router.get('/:id/edit', (req, res) => {
+  const userId = req.user._id
   const id = req.params.id
-  return Todo.findById(id)
+  return Todo.findOne({ id, userId })
     .lean()
     .then((todo) => res.render('edit', { todo }))
     .catch(error => console.log(error))
@@ -37,10 +40,11 @@ router.get('/:id/edit', (req, res) => {
 
 // 資料編輯後儲存
 router.put('/:id', (req, res) => {
+  const userId = req.user._id
   const id = req.params.id
   const { isDone, name } = req.body
 
-  return Todo.findById(id)
+  return Todo.findOne({ id, userId })
     .then(todo => {
       todo.name = name
       todo.isDone = isDone === 'on'
@@ -52,8 +56,9 @@ router.put('/:id', (req, res) => {
 
 // 資料刪除
 router.delete('/:id', (req, res) => {
+  const userId = req.user._id
   const id = req.params.id
-  return Todo.findById(id)
+  return Todo.findOne({ id, userId })
     .then(todo => todo.remove())
     .then(() => res.redirect('/'))
     .catch(error => console.log(error))
