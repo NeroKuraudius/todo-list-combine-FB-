cd46f5df393cfd590aac7eba8b7351389b3e6e40
Nero <karta0706566@gmail.com>
2023-03-22 22:27:18 +0800

    feat:modify:todoSeeder

--- models/seeds/todoSeeder.js
+++ models/seeds/todoSeeder.js
@@ -1,9 +1,36 @@
-const Todo = require('../todo') // 載入 todo model
+const bcrypt = require('bcryptjs')
+if (process.env.NODE_ENV !== 'production') {
+  require('dotenv').config()
+}
+
+const Todo = require('../todo')
+const User = require('../user')
 const db = require('../../config/mongoose')
+const user = require('../user')
+
+const SEED_USER = {
+  name: 'seed',
+  email: 'seed@seed.com',
+  password: '0123456'
+}
 
 db.once('open', () => {
-  for (let i = 0; i < 10; i++) {
-    Todo.create({ name: `name-${i}` })
-  }
-  console.log('done')
+  bcrypt.genSalt(10)
+    .then(salt => bcrypt.hash(SEED_USER.password, salt))
+    .then(hash => User.create({
+      name: SEED_USER.name,
+      email: SEED_USER.email,
+      password: hash
+    }))
+    .then(user => {
+      const userId = user._id
+      return Promise.all(Array.from({ length: 10 }, (_, i) =>
+        Todo.create({ name: `name-${i}`, userId })
+      ))
+
+    })
+    .then(() => {
+      console.log('done')
+      process.exit() // 執行後要關閉Node.js環境
+    })
 })
